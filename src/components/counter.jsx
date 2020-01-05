b5b969b63eb13a5615d2492842780c5c3b35feee
ritamxy <ritamxy@gmail.com>
2020-01-05 23:15:43 +1100

    composing components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -17,7 +17,8 @@ class Counter extends Component {
   //   );
   // }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 
@@ -26,7 +27,8 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement()}
+          // onClick={this.handleIncrement({ id: 1 })}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
